0c8b213feac35e9151c14f01c6f79d4c2010f0df
tusaronline2012 <36578657+tusaronline2012@users.noreply.github.com>
2018-03-06 08:07:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/article-three', function(req, res) {
     res.send('Article three is requested and will be served');
 });
 
+app.get('/article-four', function(req, res) {
+    res.send('Article four is requested and will be served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
